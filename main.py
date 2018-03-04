7f410f42e140f736cf677798d8c8fb4524e94199
tehginger <tehgingernwg@gmail.com>
2018-03-04 14:44:31 -0700

    program actually runs.  menu border and list subwindow created

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import curses
 
 stdscr = curses.initscr()
 
-# Properly initialize screen
+# Initialize screen
 curses.noecho()
 curses.cbreak()
 curses.curs_set(0)
@@ -27,18 +27,82 @@ curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
 # Create UI
 def mainMenu():
+    # Add Top Bar (name of program, eventually will add a version number)
     stdscr.addstr(0, 0, "gitfud", curses.A_REVERSE)
+    stdscr.chgat(-1, curses.A_REVERSE)
+
+    # Add Bottom Line (instructions to user go here)
     stdscr.addstr(curses.LINES-1, 0, "Press q to quit")
 
-    stdscr.refresh()
+    # Chanage the Q to red
+    stdscr.chgat(curses.LINES-1, 6, 1, curses.A_BOLD | curses.color_pair(1))
+
+    # Create List Window
+    list_window = curses.newwin(curses.LINES-2, curses.COLS, 1, 0)
+
+    # Create sub-window for list of recipes (seperates from the border)
+    list_recipes_window = list_window.subwin(curses.LINES-6, curses.COLS-4, 3, 2)
+
+    # Load recipes
+
+    # List recipes
+    list_recipes_window.addstr("Stuff will go here later")
+
+    # Draw list border
+    list_window.box()
+
+    # refresh internal window structures
+    stdscr.noutrefresh()
+    list_window.noutrefresh()
+
+    # redraw screen
+    curses.doupdate()
+
+    # Event Loop
+    while True:
+        c = list_window.getch()
+
+        if c == ord('q') or c == ord('Q'):
+            break
+
+        # Refresh the Screen from the bottom up
+        stdscr.noutrefresh()
+        list_window.noutrefresh()
+        list_recipes_window.noutrefresh()
+        curses.doupdate()
 
     return 0
 
 
 def main():
 
+    # Initialize screen
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+
+    # Check for and begin color support
+    if curses.has_colors():
+        curses.start_color()
+
+    # enable multibyte keysignal support
+    stdscr.keypad(1)
+
+    # Set curses colors
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+
+    # Start the Main Menu
     mainMenu()
 
+    # Restore terminal settings
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.curs_set(1)
+
+    # restore normal terminal functionality
     curses.endwin()
 
     return 0
